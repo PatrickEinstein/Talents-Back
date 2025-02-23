57ffcdc8be4edc68a1c3e8f924f457ba842a70f5
PatrickEinstein <mohammedola1234@gmail.com>
2025-02-23 16:38:39 +0100

    fix

--- src/config/Jobs/OTPexpirer.ts
+++ src/config/Jobs/OTPexpirer.ts
@@ -6,6 +6,7 @@ import { Otp } from "../../entity/Otp.js";
 export const OTPexpirer = () => {
   cron.schedule("*/5 * * * *", async () => {
     console.log(`--------OTP expirer initiated-------- at ${Date.now()}`);
+    
     const otpRepo = await AppDataSource.getRepository(Otp);
     const thirtyMinutesAgo = new Date(Date.now() - 90 * 60 * 1000);
     const otpExpired = await otpRepo.find({
